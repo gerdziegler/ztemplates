c80563f9a3945c13781f79253179182f86ccb03a
gerdziegler <gerdziegler@b9f95fbe-ae0d-0410-bd2f-e8cd2c9b2b5c>
2009-11-25 11:33:47 +0000

    git-svn-id: https://svn.code.sf.net/p/ztemplates/code@80 b9f95fbe-ae0d-0410-bd2f-e8cd2c9b2b5c

--- ztemplates-yui/trunk/ztemplates-yui/src/main/java/org/ztemplates/yui/ZYuiLoaderAction.java
+++ ztemplates-yui/trunk/ztemplates-yui/src/main/java/org/ztemplates/yui/ZYuiLoaderAction.java
@@ -9,39 +9,39 @@ public class ZYuiLoaderAction extends ZResourceLoaderAction
 {
   public static final String LOADER_URL_PREFIX = "/ztemplates/yui";
 
-  public static final String ANIMATION_MIN_JS = "/animation/animation-min.js";
+  public static final String ANIMATION_MIN_JS = LOADER_URL_PREFIX + "/animation/animation-min.js";
 
-  public static final String AUTOCOMPLETE_MIN_JS = "/autocomplete/autocomplete-min.js";
+  public static final String AUTOCOMPLETE_MIN_JS = LOADER_URL_PREFIX + "/autocomplete/autocomplete-min.js";
 
-  public static final String AUTOCOMPLETE_SAM_CSS = "/autocomplete/assets/skins/sam/autocomplete.css";
+  public static final String AUTOCOMPLETE_SAM_CSS = LOADER_URL_PREFIX + "/autocomplete/assets/skins/sam/autocomplete.css";
 
-  public static final String CONNECTION_MIN_JS = "/connection/connection-min.js";
+  public static final String CONNECTION_MIN_JS = LOADER_URL_PREFIX + "/connection/connection-min.js";
 
-  public static final String CONTAINER_MIN_JS = "/container/container-min.js";
+  public static final String CONTAINER_MIN_JS = LOADER_URL_PREFIX + "/container/container-min.js";
 
-  public static final String CONTAINER_SAM_CSS = "/container/assets/skins/sam/container.css";
+  public static final String CONTAINER_SAM_CSS = LOADER_URL_PREFIX + "/container/assets/skins/sam/container.css";
 
-  public static final String DATASOURCE_MIN_JS = "/datasource/datasource-min.js";
+  public static final String DATASOURCE_MIN_JS = LOADER_URL_PREFIX + "/datasource/datasource-min.js";
 
-  public static final String FONTS_MIN_CSS = "/fonts/fonts-min.css";
+  public static final String FONTS_MIN_CSS = LOADER_URL_PREFIX + "/fonts/fonts-min.css";
 
-  public static final String GET_MIN_JS = "/get/get-min.js";
+  public static final String GET_MIN_JS = LOADER_URL_PREFIX + "/get/get-min.js";
 
-  public static final String JSON_MIN_JS = "/json/json-min.js";
+  public static final String JSON_MIN_JS = LOADER_URL_PREFIX + "/json/json-min.js";
 
-  public static final String MENU_MIN_JS = "/menu/menu-min.js";
+  public static final String MENU_MIN_JS = LOADER_URL_PREFIX + "/menu/menu-min.js";
 
-  public static final String MENU_SAM_CSS = "/menu/assets/skins/sam/menu.css";
+  public static final String MENU_SAM_CSS = LOADER_URL_PREFIX + "/menu/assets/skins/sam/menu.css";
 
-  public static final String PAGINATOR_SAM_CSS = "/paginator/assets/skins/sam/paginator.css";
+  public static final String PAGINATOR_SAM_CSS = LOADER_URL_PREFIX + "/paginator/assets/skins/sam/paginator.css";
 
-  public static final String PAGINATOR_MIN_JS = "/paginator/paginator-min.js";
+  public static final String PAGINATOR_MIN_JS = LOADER_URL_PREFIX + "/paginator/paginator-min.js";
 
-  public static final String ELEMENT_MIN_JS = "/element/element-min.js";
+  public static final String ELEMENT_MIN_JS = LOADER_URL_PREFIX + "/element/element-min.js";
 
-  public static final String UTILITIES_JS = "/utilities/utilities.js";
+  public static final String UTILITIES_JS = LOADER_URL_PREFIX + "/utilities/utilities.js";
 
-  public static final String YAHOO_DOM_EVENT_JS = "/yahoo-dom-event/yahoo-dom-event.js";
+  public static final String YAHOO_DOM_EVENT_JS = LOADER_URL_PREFIX + "/yahoo-dom-event/yahoo-dom-event.js";
 
   protected static Logger log = Logger.getLogger(ZYuiLoaderAction.class);
 
