f465ab0995240ff333c206c18917d788fa52cf6c
gerdziegler <gerdziegler@b9f95fbe-ae0d-0410-bd2f-e8cd2c9b2b5c>
2012-08-16 15:47:06 +0000

    bugfix
    git-svn-id: https://svn.code.sf.net/p/ztemplates/code@288 b9f95fbe-ae0d-0410-bd2f-e8cd2c9b2b5c

--- ztemplates/trunk/src/main/java/org/ztemplates/web/script/zscript/ZScriptDefinitions.java
+++ ztemplates/trunk/src/main/java/org/ztemplates/web/script/zscript/ZScriptDefinitions.java
@@ -1,10 +1,14 @@
 package org.ztemplates.web.script.zscript;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.ztemplates.commons.ZIObjectFactory;
 import org.ztemplates.render.ZExpose;
 import org.ztemplates.render.ZRenderer;
 import org.ztemplates.render.velocity.ZVelocityRenderer;
+import org.ztemplates.web.ZIRenderService;
+import org.ztemplates.web.ZTemplates;
 
 @ZRenderer(value = ZVelocityRenderer.class, mimeType = "text/javascript")
 public class ZScriptDefinitions
@@ -13,13 +17,25 @@ public class ZScriptDefinitions
   final List<ZIJavaScriptDefinition> definitions;
 
   @ZExpose
+  final List<String> definitionsContent;
+
+  @ZExpose
   String prefix;
 
 
   public ZScriptDefinitions(String prefix,
-      List<ZIJavaScriptDefinition> definitions)
+      List<ZIJavaScriptDefinition> definitions) throws Exception
   {
     this.prefix = prefix;
     this.definitions = definitions;
+    this.definitionsContent = new ArrayList<String>();
+    ZIObjectFactory objectFactory = ZTemplates.getApplicationService().getObjectFactory();
+    ZIRenderService renderService = ZTemplates.getRenderService();
+    for (ZIJavaScriptDefinition def : definitions)
+    {
+      Object instance = objectFactory.newInstance(def.getClazz());
+      String rendered = renderService.render(instance);
+      this.definitionsContent.add(rendered);
+    }
   }
 }
